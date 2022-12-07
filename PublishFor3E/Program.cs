be4e977285f878e8b8162efebb28c149b79f76b7
Jon Saffron <github@saffron.me.uk>
2022-12-07 14:53:29 +0000

    initial commit

---
+++ PublishFor3E/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+
+// https://www.codeproject.com/Tips/1063552/IIS-Application-Pool-Operations-via-Csharp
+
+namespace PublishFor3E
+    {
+    internal static class Program
+        {
+        public static int Main(string[] args)
+            {
+            try
+                {
+                Console.WriteLine("Publish (and be damned) on a 3E environment");
+                Console.WriteLine();
+                if (args.Length == 0)
+                    {
+                    Console.WriteLine($"Usage: {Assembly.GetExecutingAssembly().GetName().Name} <environment> [wapi,wapi...]");
+                    Console.WriteLine($"Where <environment> is a url like http://my3eserver/TE_3E_{DateTime.Today.DayOfWeek}/");
+                    Console.WriteLine("and [wapi,wapi...] is an optional list of wapi servers to recycle.");
+                    Console.WriteLine("If no list of wapis is provided, an attempt will be made to automatically determine which they are.");
+                    return 0;
+                    }
+
+                var argsQueue = new Queue<string>(args);
+                Uri enteredUrl;
+                string environment;
+                try
+                    {
+                    enteredUrl = new Uri(argsQueue.Dequeue(), UriKind.Absolute);
+                    if (enteredUrl.Segments.Length < 2)
+                        throw new InvalidOperationException("URL is too short");
+                    environment = enteredUrl.AbsolutePath.Split('/')[1];
+                    if (!environment.StartsWith("TE_3E_", StringComparison.OrdinalIgnoreCase))
+                        throw new InvalidOperationException("URL doesn't look like a 3e environment as the path doesn't start TE_3E_");
+                    }
+                catch (Exception ex)
+                    {
+                    Console.WriteLine("Invalid URL to an environment specified: " + ex.Message);
+                    return 1;
+                    }
+                
+                Console.WriteLine($"Publishing on environment {environment}");
+                var baseUrl = new Uri(enteredUrl, $"/{environment}/");
+
+                List<string> wapis;
+                switch (argsQueue.Count)
+                    {
+                    case 0:
+                        Console.WriteLine($"Discovering WAPI servers for {environment}:");
+                        wapis = GetPossibleWapis(baseUrl).ToList();
+                        break;
+                    case 1: 
+                        wapis = ExtractWapiList(argsQueue.Dequeue()).ToList();
+                        Console.WriteLine($"WAPI servers specified: {string.Join(", ", wapis)}");
+                        break;
+                    default:
+                        wapis = argsQueue.ToList();
+                        Console.WriteLine($"WAPI servers specified: {string.Join(", ", wapis)}");
+                        break;
+                    }
+
+                Console.WriteLine();
+                var publisher = new Publisher(baseUrl, wapis);
+                bool publishSucceeded = publisher.TryPublish();
+                Console.WriteLine();
+                Console.WriteLine(publishSucceeded ? "Publish succeeded" : "Publish failed");
+
+                return publishSucceeded ? 0 : 1;
+                }
+            catch (Exception ex)
+                {
+                Console.WriteLine($"Publish failed: {ex.Message}");
+                return 1;
+                }
+            }
+
+        private static IEnumerable<string> GetPossibleWapis(Uri baseUri)
+            {
+            var result = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            var serviceInfo = GetServiceInfo(baseUri);
+            result.Add(serviceInfo.Server);
+
+            var environment = baseUri.AbsolutePath.Split('/')[1];
+
+            result.UnionWith(GetWapis(baseUri, serviceInfo.Version, environment));
+            return result;
+            }
+
+        private static ServiceInfo GetServiceInfo(Uri baseUri)
+            {
+            Console.Write("Getting ServiceInfo...");
+            try
+                {
+                string response = CallDesignerService(baseUri, "ServiceInfo");
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(response);
+                Debug.Assert(xmlDoc.DocumentElement != null);
+                var server = xmlDoc.DocumentElement!.GetAttribute("Server");
+                var version = Version.Parse(xmlDoc.DocumentElement.GetAttribute("Version"));
+                Console.WriteLine($"Server: {server}, Version: {version}");
+                return new ServiceInfo { Server = server, Version = version };
+                }
+            catch (Exception ex)
+                {
+                Console.WriteLine("Failed: " + ex.Message);
+                throw new InvalidOperationException("Cannot continue - WAPI servers cannot be determined.");
+                }
+            }
+
+        private static string CallDesignerService(Uri baseUri, string serviceName)
+            {
+            var credentialCache = new CredentialCache
+                {
+                    { baseUri, "Negotiate", CredentialCache.DefaultNetworkCredentials }
+                };
+
+            var handler = new HttpClientHandler { Credentials = credentialCache, PreAuthenticate = true };
+            var httpClient = new HttpClient(handler);
+            httpClient.BaseAddress = new Uri(baseUri, "services/DesignerService.asmx/");
+
+            var response = httpClient.GetAsync(serviceName).Result;
+            response.EnsureSuccessStatusCode();
+
+            var xml = new XmlDocument();
+            xml.LoadXml(response.Content.ReadAsStringAsync().Result);
+            var result = xml.DocumentElement!.InnerText;
+            return result;
+            }
+
+        private static IEnumerable<string> GetWapis(Uri baseUri, Version version, string environment)
+            {
+            Console.Write("Getting notification server list...");
+            // ReSharper disable once StringLiteralTypo
+            string uri = (version.Major == 2 ? "web/ui" :  "web") + "/TransactionService.asmx";
+            var credentialCache = new CredentialCache
+                {
+                    { baseUri, "Negotiate", CredentialCache.DefaultNetworkCredentials }
+                };
+
+            var handler = new HttpClientHandler { Credentials = credentialCache, PreAuthenticate = true };
+            using (var request = new HttpClient(handler))
+                {
+                request.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
+                request.DefaultRequestHeaders.Add("SOAPAction", "\"http://tempuri.org//ServiceExecuteProcess/GetArchetypeData\"");
+                request.BaseAddress = baseUri;
+                var content = new StringContent(GetServersXml(environment), Encoding.UTF8, "text/xml");
+                try
+                    {
+                    var response = request.PostAsync(uri, content).Result;
+                    response.EnsureSuccessStatusCode();
+
+                    var result = ExtractServerNames(response.Content.ReadAsStringAsync().Result);
+                    Console.WriteLine(result.Any() ? string.Join(",", result) : "no likely servers found");
+                    return result;
+                    }
+                catch (Exception ex)
+                    {
+                    Console.WriteLine("Failed: " + ex.Message);
+                    return new string[] { };
+                    }
+                }
+            }
+
+        private static string[] ExtractServerNames(string soapResponse)
+            {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(soapResponse);
+            var xnm = new XmlNamespaceManager(xmlDoc.NameTable);
+            xnm.AddNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/");
+            xnm.AddNamespace("r", "http://tempuri.org//ServiceExecuteProcess");
+
+            var dataElement = (XmlElement?) xmlDoc.SelectSingleNode("soap:Envelope/soap:Body/r:GetArchetypeDataResponse/r:GetArchetypeDataResult", xnm);
+            if (dataElement == null)
+                return new string[] { };
+            xmlDoc.LoadXml(dataElement.InnerText);
+            var serverElements = xmlDoc.SelectNodes("Data/NxNtfServer/ServerName");
+            Debug.Assert(serverElements != null);
+            return serverElements.Cast<XmlElement>().Select(element => element.InnerText).ToArray();
+            }
+
+        private static string GetServersXml(string environment)
+            {
+            var xoql = Resources.GetServers.Replace("%%Environment%%", environment);
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xoql);
+            Debug.Assert(xmlDoc.DocumentElement != null, "xmlDoc.DocumentElement != null");
+            var result = xmlDoc.DocumentElement!.OuterXml;
+            return result;
+            }
+
+        private static IEnumerable<string> ExtractWapiList(string wapiList)
+            {
+            var separatorPosition = wapiList.IndexOfAny("|,;".ToCharArray());
+            if (separatorPosition == -1)
+                {
+                // assume only one wapi
+                yield return wapiList;
+                yield break;
+                }
+
+            var separator = wapiList[separatorPosition];
+            var items = wapiList.Split(new [] {separator}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in items)
+                {
+                yield return item;
+                }
+            }
+
+        private struct ServiceInfo
+            {
+            public string Server;
+            public Version Version;
+            }
+        }
+    }
---
+++ PublishFor3E/Publisher.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Xml;
+
+namespace PublishFor3E
+    {
+    internal class Publisher
+        {
+        private readonly Uri _baseUri;
+        private readonly HashSet<string> _wapis;
+        private readonly string _environment;
+        private readonly HttpClient _httpClient;
+
+        public Publisher(Uri baseUri, IEnumerable<string> wapis)
+            {
+            _baseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+            if (baseUri.Segments.Length < 2)
+                throw new ArgumentOutOfRangeException(nameof(baseUri), "Invalid environment URL");
+
+            _wapis = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            this._wapis.UnionWith(wapis);
+            this._environment = baseUri.AbsolutePath.Split('/')[1];
+            if (!this._environment.StartsWith("TE_3E_"))
+                throw new ArgumentOutOfRangeException(nameof(baseUri), "Invalid environment URL");
+            this._httpClient = BuildHttpClient();
+            }
+
+        public bool TryPublish()
+            {
+            Console.WriteLine($"Checking WAPI servers on {this._environment}");
+            var wapisAndPools = GetRunningAppPools().ToList();
+            SwitchOffRunningWapis(wapisAndPools);
+            StartPublish();
+            Console.WriteLine("Publishing started");
+            var publishResult = MonitorPublishingState();
+            TurnAppPoolsBackOn(wapisAndPools);
+            return publishResult == State.Succeeded;
+            }
+
+        private IEnumerable<WapiPool> GetRunningAppPools()
+            {
+            foreach (var wapi in this._wapis)
+                {
+                string? appPool = GetPoolForWapi(wapi);
+                if (appPool != null)
+                    {
+                    var result = new WapiPool { WapiServer = wapi, AppPool = appPool };
+                    if (GetAppPoolState(result) == AppPoolState.Started)
+                        {
+                        yield return result;
+                        }
+                    }
+                }
+            }
+
+        private string? GetPoolForWapi(string wapi)
+            {
+            // Connection options for WMI object
+            ConnectionOptions options = new ConnectionOptions
+                {
+                // Packet Privacy means authentication with encrypted connection.
+                Authentication = AuthenticationLevel.PacketPrivacy,
+
+                // EnablePrivileges : Value indicating whether user privileges 
+                // need to be enabled for the connection operation. 
+                // This property should only be used when the operation performed 
+                // requires a certain user privilege to be enabled.
+                EnablePrivileges = true
+                };
+
+            // Connect to IIS WMI namespace \\root\\MicrosoftIISv2
+            ManagementScope scope = new ManagementScope($@"\\{wapi}\root\MicrosoftIISv2", options);
+
+            // Query IIS WMI property 
+            ObjectQuery objectQuery = new ObjectQuery("SELECT * FROM IIsWebVirtualDirSetting");
+
+            // Search and collect details thru WMI methods
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, objectQuery);
+            ManagementObjectCollection virtualDirectorySettings = searcher.Get();
+
+            string applicationName = $"W3SVC/1/ROOT/{this._environment}/Web";
+            // ReSharper disable once PossibleInvalidCastExceptionInForeachLoop
+            foreach (ManagementObject virtualDirectorySetting in virtualDirectorySettings)
+                {
+                string? virtualDirName = virtualDirectorySetting["Name"] as string;
+                //string path = virtualDirectorySetting["Path"] as string;
+                string? appPool = virtualDirectorySetting["AppPoolId"] as string;
+
+                if (string.Equals(virtualDirName, applicationName, StringComparison.OrdinalIgnoreCase)) 
+                    {
+                    return appPool;
+                    }
+                }
+            return null;
+            }
+
+        private static AppPoolState GetAppPoolState(WapiPool wapiPool)
+            {
+            // Connection options for WMI object
+            ConnectionOptions options = new ConnectionOptions
+                {
+                // Packet Privacy means authentication with encrypted connection.
+                Authentication = AuthenticationLevel.PacketPrivacy,
+
+                // EnablePrivileges : Value indicating whether user privileges 
+                // need to be enabled for the connection operation. 
+                // This property should only be used when the operation performed 
+                // requires a certain user privilege to be enabled.
+                EnablePrivileges = true
+                };
+
+            // Connect to IIS WMI namespace \\root\\MicrosoftIISv2
+            ManagementScope scope = new ManagementScope($@"\\{wapiPool.WapiServer}\root\MicrosoftIISv2", options);
+
+            // Query IIS WMI property
+            ObjectQuery objectQuery = new ObjectQuery("SELECT * FROM IISApplicationPoolSetting");
+
+            // Search and collect details thru WMI methods
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, objectQuery);
+            ManagementObjectCollection applicationPoolSettings = searcher.Get();
+
+            // ReSharper disable once PossibleInvalidCastExceptionInForeachLoop
+            foreach (ManagementObject applicationPoolSetting in applicationPoolSettings)
+                {
+                // full name is of form /W3SVC/AppPools/DefaultAppPool
+                string? fullAppName = applicationPoolSetting["Name"] as string;
+                string? partialAppName = fullAppName?.Split('/')[2];
+                AppPoolState appPoolState = (AppPoolState) applicationPoolSetting["AppPoolState"];
+
+                if (string.Equals(partialAppName, wapiPool.AppPool, StringComparison.OrdinalIgnoreCase)) 
+                    {
+                    return appPoolState;
+                    }
+                }
+            return AppPoolState.Unknown;
+            }
+
+        private static void SwitchOffRunningWapis(IEnumerable<WapiPool> wapisAndPools)
+            {
+            foreach (var item in wapisAndPools)
+                {
+                Console.Write($"Stopping app pool {item.AppPool} on {item.WapiServer}...");
+                try
+                    {
+                    SendControlRequestToAppPool(item, "Stop");
+                    Console.WriteLine("done");
+                    }
+                catch (Exception ex)
+                    {
+                    Console.WriteLine($"failed {ex.Message}");
+                    }
+                }
+            }
+
+        private static void TurnAppPoolsBackOn(IEnumerable<WapiPool> wapisAndPools)
+            {
+            foreach (var item in wapisAndPools)
+                {
+                Console.Write($"Starting app pool {item.AppPool} on {item.WapiServer}...");
+                try
+                    {
+                    SendControlRequestToAppPool(item, "Start");
+                    Console.WriteLine("done");
+                    }
+                catch (Exception ex)
+                    {
+                    Console.WriteLine($"failed {ex.Message}");
+                    }
+                }
+            }
+
+        private static void SendControlRequestToAppPool(WapiPool wapiPool, string action)
+            {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+            if (!action.Equals("Start", StringComparison.OrdinalIgnoreCase) 
+                && !action.Equals("Stop", StringComparison.OrdinalIgnoreCase)
+                && !action.Equals("Recycle", StringComparison.OrdinalIgnoreCase))
+                {
+                throw new ArgumentOutOfRangeException(nameof(action), "Specify Start, Stop or Recycle");
+                }
+
+            ConnectionOptions options = new ConnectionOptions
+                {
+                Authentication = AuthenticationLevel.PacketPrivacy,
+                EnablePrivileges = true
+                };
+            ManagementScope scope = new ManagementScope($@"\\{wapiPool.WapiServer}\root\MicrosoftIISv2", options);
+
+            // IIS WMI object IISApplicationPool to perform actions on IIS Application Pool
+            ObjectQuery objectQuery = new ObjectQuery("SELECT * FROM IISApplicationPool");
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, objectQuery);
+            ManagementObjectCollection applicationPools = searcher.Get();
+            // ReSharper disable once PossibleInvalidCastExceptionInForeachLoop
+            foreach (ManagementObject applicationPool in applicationPools)
+                {
+                string? fullAppName = applicationPool["Name"] as string;
+                string? partialAppName = fullAppName?.Split('/')[2];
+                if (string.Equals(partialAppName, wapiPool.AppPool, StringComparison.OrdinalIgnoreCase))
+                    {
+                    applicationPool.InvokeMethod(action, null!);
+                    }
+                }
+            }
+
+        private HttpClient BuildHttpClient()
+            {
+            var credentialCache = new CredentialCache
+                {
+                    { this._baseUri, "Negotiate", CredentialCache.DefaultNetworkCredentials }
+                };
+
+            var handler = new HttpClientHandler { Credentials = credentialCache, PreAuthenticate = true };
+            var result = new HttpClient(handler);
+            result.BaseAddress = new Uri(this._baseUri, "services/DesignerService.asmx/");
+            return result;
+            }
+
+        private void StartPublish()
+            {
+            var response = CallDesignerService("PublishAll");
+            if (response != "started")
+                {
+                if (string.IsNullOrWhiteSpace(response))
+                    {
+                    response = "(no response)";
+                    }
+                throw new InvalidOperationException($"Could not start publishing: {response}");
+                }
+            }
+
+        private State MonitorPublishingState()
+            {
+            string? lastResponse = null;
+            State state;
+            do
+                {
+                Thread.Sleep(250);
+                string status = CallDesignerService("CheckPublishingState");
+                string newInfo = ExtractNewInfo(status, out state);
+                if (newInfo != lastResponse)
+                    {
+                    Console.WriteLine(newInfo);
+                    }
+                lastResponse = newInfo;
+                } while (state == State.Ongoing);
+
+            return state;
+            }
+
+        private string CallDesignerService(string serviceName)
+            {
+            var response = this._httpClient.GetAsync(serviceName).Result;
+            response.EnsureSuccessStatusCode();
+
+            var xml = new XmlDocument();
+            xml.LoadXml(response.Content.ReadAsStringAsync().Result);
+            var result = xml.DocumentElement!.InnerText;
+            return result;
+            }
+
+        private static string ExtractNewInfo(string status, out State state)
+            {
+            if (status.StartsWith("."))
+                {
+                state = State.Ongoing;
+                return status.Substring(1);
+                }
+
+            if (status.StartsWith("**"))
+                {
+                state = State.Failed;
+                return status.Substring(2);
+                }
+
+            if (status.StartsWith("*!"))
+                {
+                state = State.Succeeded;
+                return status.Substring(2);
+                }
+
+            throw new InvalidOperationException($"Invalid response: {status}");
+            }
+
+        private struct WapiPool
+            {
+            public string WapiServer;
+            public string AppPool;
+            }
+
+        
+        private enum AppPoolState
+            {
+            Unknown = 0,
+            // ReSharper disable UnusedMember.Local
+            Starting = 1,
+            Started = 2,
+            Stopping = 3,
+            Stopped = 4
+            // ReSharper restore UnusedMember.Local
+            }
+
+        private enum State
+            {
+            Ongoing,
+            Succeeded,
+            Failed
+            }
+        }
+    }
